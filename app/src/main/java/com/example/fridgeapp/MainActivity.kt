6379a5027ef80ff8a971ede6155cfc8c01c814fa
Diana Lam <dianal@middlebury.edu>
2020-10-12 21:44:23 -0500

    created second activity with listview and linked with start button

--- app/src/main/java/com/example/fridgeapp/MainActivity.kt
+++ app/src/main/java/com/example/fridgeapp/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.fridgeapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
+
+const val moveToListActivity = "com.example.fridgeapp.ListActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +19,24 @@ class MainActivity : AppCompatActivity() {
         actionBar!!.title = "Fridge Friend"
 
         createListBtn.setOnClickListener{
-            val intent = Intent(this, CreateList::class.java)
+            val intent = Intent(this, ListActivity::class.java)
             startActivity(intent)
         }
+
+        /*
+
+        val listView = findViewById<ListView>(R.id.listView)
+        var foodList = ArrayList<String>()
+
+        foodList.add("Avocado")
+        foodList.add("Orange Juice")
+        foodList.add("Bread")
+
+        var foodArrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,foodList)
+
+        listView.adapter = foodArrayAdapter
+
+         */
+
     }
 }
